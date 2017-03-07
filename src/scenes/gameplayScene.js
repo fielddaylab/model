0c7cc6a5007d53f86bfca5b20377a953d8ff9889
Phildo <Phildo211@gmail.com>
2017-03-07 14:30:57 -0600

    removed pools

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -12,7 +12,6 @@ var GamePlayScene = function(game, stage)
   var screen_cam;
   var work_cam;
 
-  var pools;
   var modules;
   var dragging_obj;
   var full_pause;
@@ -22,7 +21,6 @@ var GamePlayScene = function(game, stage)
   var max_tick_i;
   var tick_i;
 
-  var add_pool_btn;
   var add_module_btn;
   var pause_btn;
   var advance_btn;
@@ -90,14 +88,7 @@ var GamePlayScene = function(game, stage)
   var print_template = function()
   {
     var str = "";
-    str += "{\"pools\":[";
-    for(var i = 0; i < pools.length; i++)
-    {
-      var p = pools[i];
-      str += "{\"v\":"+p.v+",\"locked\":"+p.locked+",\"wx\":"+p.wx+",\"wy\":"+p.wy+",\"ww\":"+p.ww+",\"wh\":"+p.wh+"}";
-      if(i < pools.length-1) str += ",";
-    }
-    str += "],\"modules\":[";
+    str += "{\"modules\":[";
     for(var i = 0; i < modules.length; i++)
     {
       var m = modules[i];
@@ -105,17 +96,13 @@ var GamePlayScene = function(game, stage)
       var adder = -1;
       if(m.input_dongle.attachment)
       {
-        for(var j = 0; j < pools.length; j++)
-          if(m.input_dongle.attachment == pools[j]) input = j;
         for(var j = 0; j < modules.length; j++)
-          if(m.input_dongle.attachment == modules[j]) input = pools.length+j;
+          if(m.input_dongle.attachment == modules[j]) input = j;
       }
       if(m.adder_dongle.attachment)
       {
-        for(var j = 0; j < pools.length; j++)
-          if(m.adder_dongle.attachment == pools[j]) adder = j;
         for(var j = 0; j < modules.length; j++)
-          if(m.adder_dongle.attachment == modules[j]) adder = pools.length+j;
+          if(m.adder_dongle.attachment == modules[j]) adder = j;
       }
       str += "{\"v\":"+m.v+",\"locked\":"+m.locked+",\"wx\":"+m.wx+",\"wy\":"+m.wy+",\"ww\":"+m.ww+",\"wh\":"+m.wh+",\"input\":"+input+",\"adder\":"+adder+"}";
       if(i < modules.length-1) str += ",";
@@ -129,18 +116,8 @@ var GamePlayScene = function(game, stage)
   var load_template = function(template)
   {
     var t = JSON.parse(template);
-    pools = [];
     modules = [];
 
-    for(var i = 0; i < t.pools.length; i++)
-    {
-      var tp = t.pools[i];
-      var p = new pool(tp.wx,tp.wy,tp.ww,tp.wh);
-      screenSpace(work_cam,canv,p);
-      p.v = tp.v;
-      p.locked = tp.locked;
-      pools.push(p);
-    }
     for(var i = 0; i < t.modules.length; i++)
     {
       var tm = t.modules[i];
@@ -154,19 +131,9 @@ var GamePlayScene = function(game, stage)
     {
       var tm = t.modules[i];
       if(tm.input >= 0)
-      {
-        if(tm.input >= pools.length)
-          modules[i].input_dongle.attachment = modules[tm.input-pools.length];
-        else
-          modules[i].input_dongle.attachment = pools[tm.input];
-      }
+        modules[i].input_dongle.attachment = modules[tm.input];
       if(tm.adder >= 0)
-      {
-        if(tm.adder >= pools.length)
-          modules[i].adder_dongle.attachment = modules[tm.adder-pools.length];
-        else
-          modules[i].adder_dongle.attachment = pools[tm.adder];
-      }
+        modules[i].adder_dongle.attachment = modules[tm.adder];
     }
 
     work_cam.wx = 0;
@@ -266,117 +233,7 @@ var GamePlayScene = function(game, stage)
         if(self.src != modules[i] && doEvtWithinBB(evt, modules[i]))
           self.attachment = modules[i];
       }
-      for(var i = 0; i < pools.length; i++)
-      {
-        if(self.src != pools[i] && doEvtWithinBB(evt, pools[i]))
-          self.attachment = pools[i];
-      }
     }
-  }
-
-  var pool = function(wx,wy,ww,wh)
-  {
-    var self = this;
-
-    self.wx = wx;
-    self.wy = wy;
-    self.ww = ww;
-    self.wh = wh;
-    self.x = 0;
-    self.y = 0;
-    self.w = 0;
-    self.h = 0;
-    screenSpace(work_cam,canv,self);
-
-    self.title = "blarb";
-    self.color = "#FFFF00";
-    self.damp_color = "rgba(200,200,200,0.5)";
-
-    self.v = 0;
-    self.v_temp = 0;
-    self.range = 100;
-    self.locked = 0;
-
-    self.plot = [];
-
-    self.lock_dongle = new dongle(0,-20,dongle_img.width/2,self);
-    self.lock_dongle.shouldDrag = function(evt)
-    {
-      if(dragging_obj) return false;
-      if(distsqr(self.x+self.w/2+self.lock_dongle.off.x,self.y+self.w/2+self.lock_dongle.off.y,evt.doX,evt.doY) < self.lock_dongle.r*self.lock_dongle.r)
-        self.locked = !self.locked;
-      return false;
-    }
-
-    self.v_dongle = new dongle(0,0,dongle_img.width/2,self);
-    self.v_dongle.drag = function(evt)
-    {
-      self.v_dongle.delta = self.v_dongle.drag_start_y-evt.doY;
-      self.v_temp = clamp(0,self.range,self.v+self.v_dongle.delta);
-    }
-    self.v_dongle.dragFinish = function(evt)
-    {
-      if(dragging_obj == self.v_dongle) dragging_obj = 0;
-      drag_pause = false;
-      self.v = self.v_temp;
-    }
-
-    //the module itself
-    self.shouldDrag = function(evt)
-    {
-      if(dragging_obj && dragging_obj != self) return false;
-      return doEvtWithinBB(evt,self);
-    }
-    self.dragStart = function(evt)
-    {
-      dragging_obj = self;
-      self.drag_start_x = evt.doX-self.x;
-      self.drag_start_y = evt.doY-self.y;
-    }
-    self.drag = function(evt)
-    {
-      self.x = evt.doX-self.drag_start_x;
-      self.y = evt.doY-self.drag_start_y;
-      worldSpaceCoords(work_cam,canv,self);
-    }
-    self.dragFinish = function(evt)
-    {
-      if(dragging_obj == self) dragging_obj = 0;
-    }
-
-    self.draw = function()
-    {
-      //title
-      //ctx.fillStyle = "#000000";
-      //ctx.fillText(self.title,self.x,self.y-10);
-
-      //fill
-      ctx.fillStyle = "#FFFFFF";
-      ctx.fillRect(self.x,self.y,self.w,self.h);
-      ctx.fillStyle = self.color;
-      var p = self.v/self.range;
-      ctx.fillRect(self.x,self.y+self.h*(1-p),self.w,self.h*p);
-      if(self.v_dongle.dragging)
-      {
-        ctx.fillStyle = self.damp_color;
-        var p = self.v_temp/self.range;
-        ctx.fillRect(self.x,self.y+self.h*(1-p),self.w,self.h*p);
-      }
-
-      //v_dongle
-      ctx.fillStyle = "#000000";
-      ctx.drawImage(dongle_img,self.x+self.w/2+self.v_dongle.off.x-self.v_dongle.r,self.y+self.h/2+self.v_dongle.off.y-self.v_dongle.r,self.v_dongle.r*2,self.v_dongle.r*2);
-      if(self.v_dongle.dragging) ctx.fillText(self.v_temp,self.x+self.w/2+self.v_dongle.off.x-self.v_dongle.r/2,self.y+self.h/2+self.v_dongle.off.y+self.v_dongle.r/2);
-      else                       ctx.fillText(self.v     ,self.x+self.w/2+self.v_dongle.off.x-self.v_dongle.r/2,self.y+self.h/2+self.v_dongle.off.y+self.v_dongle.r/2);
-
-      //lock_dongle
-      ctx.fillStyle = "#000000";
-      //ctx.drawImage(dongle_img,self.x+self.w/2+self.lock_dongle.off.x-self.lock_dongle.r,self.y+self.h/2+self.lock_dongle.off.y-self.lock_dongle.r,self.lock_dongle.r*2,self.lock_dongle.r*2);
-      if(self.locked) ctx.fillText("o",self.x+self.w/2+self.lock_dongle.off.x-self.lock_dongle.r/2,self.y+self.h/2+self.lock_dongle.off.y+self.lock_dongle.r/2);
-      else            ctx.fillText("+",self.x+self.w/2+self.lock_dongle.off.x-self.lock_dongle.r/2,self.y+self.h/2+self.lock_dongle.off.y+self.lock_dongle.r/2);
-
-    }
-
   }
 
   var module = function(wx,wy,ww,wh)
@@ -399,7 +256,8 @@ var GamePlayScene = function(game, stage)
 
     self.v = 0;
     self.v_temp = 0;
-    self.range = 10;
+    self.min = -10;
+    self.max =  100;
     self.locked = 0;
 
     self.plot = [];
@@ -417,7 +275,7 @@ var GamePlayScene = function(game, stage)
     self.v_dongle.drag = function(evt)
     {
       self.v_dongle.delta = (self.v_dongle.drag_start_y-evt.doY)/10;
-      self.v_temp = clamp(-self.range,self.range,self.v+self.v_dongle.delta);
+      self.v_temp = clamp(self.min,self.max,self.v+self.v_dongle.delta);
     }
     self.v_dongle.dragFinish = function(evt)
     {
@@ -516,7 +374,7 @@ var GamePlayScene = function(game, stage)
       }
 
       //adder_dongle
-      ctx.lineWidth = abs(self.v/self.range)*dongle_img.width/2;
+      ctx.lineWidth = self.v/max(abs(self.min),abs(self.max))*dongle_img.width/2;
       if(self.v > 0)
         ctx.strokeStyle = "#6666FF";
       else if(self.v < 0)
@@ -532,7 +390,7 @@ var GamePlayScene = function(game, stage)
       if(self.v_dongle.dragging)
       {
         ctx.globalAlpha = 0.5;
-        ctx.lineWidth = abs(self.v_temp/self.range)*dongle_img.width/2;
+        ctx.lineWidth = self.v_temp/max(abs(self.min),abs(self.max))*dongle_img.width/2;
         if(self.v_temp*self.v > 0) //neither zero, same sign
           ctx.strokeStyle = "#666666";
         else if(self.v_temp > 0)
@@ -662,7 +520,6 @@ var GamePlayScene = function(game, stage)
     screen_cam = {wx:0,wy:0,ww:2,wh:canv.height/canv.width*2,x:0,y:0,w:0,y:0};
     work_cam   = {wx:0,wy:0,ww:2,wh:canv.height/canv.width*2,x:0,y:0,w:0,y:0};
 
-    pools = [];
     modules = [];
     dragging_obj = 0;
     full_pause = false;
@@ -674,35 +531,17 @@ var GamePlayScene = function(game, stage)
 
     load_template_i = 0;
     templates = [];
-    /*empty*/            templates.push("{\"pools\":[],\"modules\":[]}");
-    /*feedback loop*/    templates.push("{\"pools\":[{\"v\":0,\"locked\":true,\"wx\":-0.35,\"wy\":0.18749999999999994,\"ww\":0.1,\"wh\":0.1},{\"v\":0,\"locked\":true,\"wx\":0.1656249999999999,\"wy\":0.184375,\"ww\":0.1,\"wh\":0.1}],\"modules\":[{\"v\":1.1,\"locked\":0,\"wx\":-0.13124999999999998,\"wy\":0.38125,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":1.1,\"locked\":0,\"wx\":-0.13124999999999998,\"wy\":0.021874999999999978,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1}]}");
-    /*normalizing loop*/ templates.push("{\"pools\":[{\"v\":55.56,\"locked\":0,\"wx\":-0.10624999999999996,\"wy\":0.06874999999999998,\"ww\":0.1,\"wh\":0.1},{\"v\":50,\"locked\":true,\"wx\":0.5125,\"wy\":0.09687499999999999,\"ww\":0.1,\"wh\":0.1},{\"v\":50,\"locked\":0,\"wx\":-0.44687500000000013,\"wy\":0.43125,\"ww\":0.1,\"wh\":0.1}],\"modules\":[{\"v\":-1,\"locked\":0,\"wx\":0.1875,\"wy\":0.11249999999999999,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":1,\"locked\":0,\"wx\":-0.43437499999999996,\"wy\":0.19374999999999998,\"ww\":0.15625,\"wh\":0.15625,\"input\":2,\"adder\":0},{\"v\":0.9,\"locked\":0,\"wx\":0.1968749999999999,\"wy\":-0.10312500000000002,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1}]}");
-    /*cycle*/            templates.push("{\"pools\":[{\"v\":0,\"locked\":0,\"wx\":-0.31875,\"wy\":0.184375,\"ww\":0.1,\"wh\":0.1},{\"v\":70.6,\"locked\":0,\"wx\":0.2437499999999999,\"wy\":0.175,\"ww\":0.1,\"wh\":0.1}],\"modules\":[{\"v\":-0.3,\"locked\":0,\"wx\":-0.078125,\"wy\":0.384375,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":0.3,\"locked\":0,\"wx\":-0.050000000000000044,\"wy\":0.003124999999999989,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1},{\"v\":10,\"locked\":0,\"wx\":-0.6,\"wy\":0.21875,\"ww\":0.15625,\"wh\":0.15625,\"input\":-1,\"adder\":0},{\"v\":-10,\"locked\":0,\"wx\":0.48124999999999996,\"wy\":0.203125,\"ww\":0.15625,\"wh\":0.15625,\"input\":-1,\"adder\":1}]}");
-    /*proportion cycle*/ templates.push("{\"pools\":[{\"v\":100,\"locked\":0,\"wx\":-0.31875,\"wy\":0.184375,\"ww\":0.1,\"wh\":0.1},{\"v\":50,\"locked\":0,\"wx\":0.2437499999999999,\"wy\":0.175,\"ww\":0.1,\"wh\":0.1}],\"modules\":[{\"v\":-0.4,\"locked\":0,\"wx\":-0.078125,\"wy\":0.384375,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":0.1,\"locked\":0,\"wx\":-0.050000000000000044,\"wy\":0.003124999999999989,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1},{\"v\":0.3,\"locked\":0,\"wx\":-0.6,\"wy\":0.21875,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":0},{\"v\":-0.2,\"locked\":0,\"wx\":0.48124999999999996,\"wy\":0.203125,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":1}]}");
+    /*empty*/            templates.push("{\"modules\":[]}");
+    /*feedback loop*/    templates.push("{\"modules\":[{\"v\":0,\"locked\":true,\"wx\":-0.35,\"wy\":0.18749999999999994,\"ww\":0.15625,\"wh\":0.15625},{\"v\":0,\"locked\":true,\"wx\":0.1656249999999999,\"wy\":0.184375,\"ww\":0.15625,\"wh\":0.15625},{\"v\":1.1,\"locked\":0,\"wx\":-0.13124999999999998,\"wy\":0.38125,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":1.1,\"locked\":0,\"wx\":-0.13124999999999998,\"wy\":0.021874999999999978,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1}]}");
+    /*normalizing loop*/ templates.push("{\"modules\":[{\"v\":55.56,\"locked\":0,\"wx\":-0.10624999999999996,\"wy\":0.06874999999999998,\"ww\":0.15625,\"wh\":0.15625},{\"v\":50,\"locked\":true,\"wx\":0.5125,\"wy\":0.09687499999999999,\"ww\":0.15625,\"wh\":0.15625},{\"v\":50,\"locked\":0,\"wx\":-0.44687500000000013,\"wy\":0.43125,\"ww\":0.15625,\"wh\":0.15625},{\"v\":-1,\"locked\":0,\"wx\":0.1875,\"wy\":0.11249999999999999,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":1,\"locked\":0,\"wx\":-0.43437499999999996,\"wy\":0.19374999999999998,\"ww\":0.15625,\"wh\":0.15625,\"input\":2,\"adder\":0},{\"v\":0.9,\"locked\":0,\"wx\":0.1968749999999999,\"wy\":-0.10312500000000002,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1}]}");
+    /*cycle*/            templates.push("{\"modules\":[{\"v\":0,\"locked\":0,\"wx\":-0.31875,\"wy\":0.184375,\"ww\":0.15625,\"wh\":0.15625},{\"v\":70.6,\"locked\":0,\"wx\":0.2437499999999999,\"wy\":0.175,\"ww\":0.15625,\"wh\":0.15625},{\"v\":-0.3,\"locked\":0,\"wx\":-0.078125,\"wy\":0.384375,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":0.3,\"locked\":0,\"wx\":-0.050000000000000044,\"wy\":0.003124999999999989,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1},{\"v\":10,\"locked\":0,\"wx\":-0.6,\"wy\":0.21875,\"ww\":0.15625,\"wh\":0.15625,\"input\":-1,\"adder\":0},{\"v\":-10,\"locked\":0,\"wx\":0.48124999999999996,\"wy\":0.203125,\"ww\":0.15625,\"wh\":0.15625,\"input\":-1,\"adder\":1}]}");
+    /*proportion cycle*/ templates.push("{\"modules\":[{\"v\":100,\"locked\":0,\"wx\":-0.31875,\"wy\":0.184375,\"ww\":0.15625,\"wh\":0.15625},{\"v\":50,\"locked\":0,\"wx\":0.2437499999999999,\"wy\":0.175,\"ww\":0.1,\"wh\":0.15625},{\"v\":-0.4,\"locked\":0,\"wx\":-0.078125,\"wy\":0.384375,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":0},{\"v\":0.1,\"locked\":0,\"wx\":-0.050000000000000044,\"wy\":0.003124999999999989,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":1},{\"v\":0.3,\"locked\":0,\"wx\":-0.6,\"wy\":0.21875,\"ww\":0.15625,\"wh\":0.15625,\"input\":0,\"adder\":0},{\"v\":-0.2,\"locked\":0,\"wx\":0.48124999999999996,\"wy\":0.203125,\"ww\":0.15625,\"wh\":0.15625,\"input\":1,\"adder\":1}]}");
 
     s_dragger = new screen_dragger();
 
-    add_pool_btn = new btn(-0.4,0.4,0.2,0.2);
-    add_pool_btn.wx = -screen_cam.ww/2+add_pool_btn.ww/2+0.1;
-    add_pool_btn.wy =  screen_cam.wh/2-add_pool_btn.wh/2-0.1;
-    screenSpace(screen_cam,canv,add_pool_btn);
-    add_pool_btn.shouldDrag = function(evt)
-    {
-      if(dragging_obj) return false;
-      if(doEvtWithinBB(evt,add_pool_btn))
-      {
-        var p = new pool(worldSpaceX(work_cam,canv,evt.doX),worldSpaceY(work_cam,canv,evt.doY),0.1,0.1);
-        screenSpace(work_cam,canv,p);
-
-        if(p.shouldDrag(evt)) { p.dragStart(evt); p.dragging = true; }
-        pools.push(p);
-      }
-      return false;
-    }
-
     add_module_btn = new btn(-0.4,0.4,0.2,0.2);
     add_module_btn.wx = -screen_cam.ww/2+add_module_btn.ww/2+0.1;
-    add_module_btn.wy =  screen_cam.wh/2-add_module_btn.wh/2-0.1-add_pool_btn.wh-0.1;
+    add_module_btn.wy =  screen_cam.wh/2-add_module_btn.wh/2-0.1;
     screenSpace(screen_cam,canv,add_module_btn);
     add_module_btn.shouldDrag = function(evt)
     {
@@ -720,7 +559,7 @@ var GamePlayScene = function(game, stage)
 
     pause_btn = new btn(-0.4,0.4,0.2,0.2);
     pause_btn.wx = -screen_cam.ww/2+pause_btn.ww/2+0.1;
-    pause_btn.wy =  screen_cam.wh/2-pause_btn.wh/2-0.1-add_pool_btn.wh-0.1-add_module_btn.wh-0.1;
+    pause_btn.wy =  screen_cam.wh/2-pause_btn.wh/2-0.1-add_module_btn.wh-0.1;
     screenSpace(screen_cam,canv,pause_btn);
     pause_btn.shouldDrag = function(evt)
     {
@@ -789,9 +628,6 @@ var GamePlayScene = function(game, stage)
     if(tick_i >= max_tick_i) tick_i = 0;
 
     tick_timer = max_tick_timer;
-    for(var i = 0; i < pools.length; i++)
-      if(pools[i].locked) pools[i].v_temp = 0;
-      else                pools[i].v_temp = pools[i].v;
     for(var i = 0; i < modules.length; i++)
       if(modules[i].locked) modules[i].v_temp = 0;
       else                  modules[i].v_temp = modules[i].v;
@@ -805,15 +641,9 @@ var GamePlayScene = function(game, stage)
       }
     }
 
-    for(var i = 0; i < pools.length; i++)
-    {
-      pools[i].v_temp = fdisp(clamp(0,pools[i].range,pools[i].v_temp),2);
-      pools[i].v = pools[i].v_temp;
-      pools[i].plot[tick_i] = pools[i].v;
-    }
     for(var i = 0; i < modules.length; i++)
     {
-      modules[i].v_temp = fdisp(clamp(-modules[i].range,modules[i].range,modules[i].v_temp),2);
+      modules[i].v_temp = fdisp(clamp(modules[i].min,modules[i].max,modules[i].v_temp),2);
       modules[i].v = modules[i].v_temp;
       modules[i].plot[tick_i] = modules[i].v;
     }
@@ -821,12 +651,6 @@ var GamePlayScene = function(game, stage)
 
   self.tick = function()
   {
-    for(var i = 0; i < pools.length; i++)
-      dragger.filter(pools[i].lock_dongle);
-    for(var i = 0; i < pools.length; i++)
-      dragger.filter(pools[i].v_dongle);
-    for(var i = 0; i < pools.length; i++)
-      dragger.filter(pools[i]);
     for(var i = 0; i < modules.length; i++)
       dragger.filter(modules[i].lock_dongle);
     for(var i = 0; i < modules.length; i++)
@@ -837,7 +661,6 @@ var GamePlayScene = function(game, stage)
       dragger.filter(modules[i].adder_dongle);
     for(var i = 0; i < modules.length; i++)
       dragger.filter(modules[i]);
-    dragger.filter(add_pool_btn);
     dragger.filter(add_module_btn);
     dragger.filter(pause_btn);
     dragger.filter(advance_btn);
@@ -868,8 +691,6 @@ var GamePlayScene = function(game, stage)
     ctx.fillStyle = "#000000";
     ctx.strokeStyle = "#000000";
     ctx.lineWidth = 1;
-    ctx.fillText("Add Pool",add_pool_btn.x+2,add_pool_btn.y+10);
-    ctx.strokeRect(add_pool_btn.x,add_pool_btn.y,add_pool_btn.w,add_pool_btn.h);
     ctx.fillText("Add Amp",add_module_btn.x+2,add_module_btn.y+10);
     ctx.strokeRect(add_module_btn.x,add_module_btn.y,add_module_btn.w,add_module_btn.h);
     ctx.fillText("Pause",pause_btn.x+2,pause_btn.y+10);
@@ -885,13 +706,9 @@ var GamePlayScene = function(game, stage)
 
     for(var i = 0; i < modules.length; i++)
       screenSpace(work_cam,canv,modules[i]);
-    for(var i = 0; i < pools.length; i++)
-      screenSpace(work_cam,canv,pools[i]);
 
     for(var i = 0; i < modules.length; i++)
       modules[i].draw_bg();
-    for(var i = 0; i < pools.length; i++)
-      pools[i].draw();
     for(var i = 0; i < modules.length; i++)
       modules[i].draw();
 
@@ -905,22 +722,6 @@ var GamePlayScene = function(game, stage)
     ctx.strokeStyle = "#000000";
     var x = 0;
     var y = 0;
-    for(var i = 0; i < pools.length; i++)
-    {
-      x = 0;
-      y = canv.height;
-      ctx.beginPath();
-      if(!isNaN(pools[i].plot[0])) y = canv.height-((pools[i].plot[0]/pools[i].range)*100);
-      ctx.moveTo(x,y);
-      for(var j = 0; j < max_tick_i; j++)
-      {
-        if(j <= tick_i) x =  j   /(max_tick_i-1)*canv.width;
-        else            x = (j-1)/(max_tick_i-1)*canv.width;
-        if(!isNaN(pools[i].plot[j])) y = canv.height-((pools[i].plot[j]/pools[i].range)*100);
-        ctx.lineTo(x,y);
-      }
-      ctx.stroke();
-    }
 
     for(var i = 0; i < modules.length; i++)
     {
